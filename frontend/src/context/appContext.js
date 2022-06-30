3c366d1912322bf896e50b4e77bfadf8b135ae79
ramansain20 <20je0766@ece.iitism.ac.in>
2022-06-30 17:53:18 +0530

    bug fix

--- frontend/src/context/appContext.js
+++ frontend/src/context/appContext.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 import React from "react";
-const SOCKET_URL = "hhttps://chipher-chat.herokuapp.com/";
+const SOCKET_URL = "https://chipher-chat.herokuapp.com";
 export const socket = io(SOCKET_URL);
 // app context
 export const AppContext = React.createContext();
